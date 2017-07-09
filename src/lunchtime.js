cc4214fb51ad1344d1cf5dbb8198793978b82dba
Rich Harris <richard.a.harris@gmail.com>
2017-07-08 22:23:40 -0400

    move option defaults

--- src/lunchtime.js
+++ src/lunchtime.js
@@ -1,9 +1,6 @@
 export default function getNextLunchtime (hours, minutes) {
-	// lunch is at 12.30
-	if (hours === undefined) hours = 12;
-	if (minutes === undefined) minutes = 30;
-
 	var lunchtime = new Date();
+
 	lunchtime.setHours(hours);
 	lunchtime.setMinutes(minutes);
 	lunchtime.setSeconds(0);
