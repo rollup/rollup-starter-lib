bd4c5c8148cd747dbf3915c9e1b78950f9fcd444
Mihal Malostanidis <qm3ster@gmail.com>
2017-10-08 02:09:26 +0300

    Update to new config format

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,11 @@ import pkg from './package.json';
 export default [
 	// browser-friendly UMD build
 	{
-		entry: 'src/main.js',
-		dest: pkg.browser,
-		format: 'umd',
+		input: 'src/main.js',
+		output: {
+			file: pkg.browser,
+			format: 'umd'
+		},
 		moduleName: 'howLongUntilLunch',
 		plugins: [
 			resolve(), // so Rollup can find `ms`
@@ -21,11 +23,11 @@ export default [
 	// builds from a single configuration where possible, using
 	// the `targets` option which can specify `dest` and `format`)
 	{
-		entry: 'src/main.js',
+		input: 'src/main.js',
 		external: ['ms'],
-		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
 		]
 	}
 ];
