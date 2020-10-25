c59fdbb11aead4c72a226880eb43854612bbcbcd
Doug A <douganderson444@gmail.com>
2020-10-25 05:06:18 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { resolve } from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
